d55db431453d3510329e85ce0fc534e0935b24ca
Cristian Rojas <cristianrojas.ing@gmail.com>
2018-09-14 16:29:22 -0400

    clase 3 constraintLayout

--- app/src/main/java/com/example/hp/miprimerlayout/MainActivity.kt
+++ app/src/main/java/com/example/hp/miprimerlayout/MainActivity.kt
@@ -6,10 +6,10 @@ import android.graphics.drawable.RippleDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import com.example.hp.miprimerlayout.R.id.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +18,6 @@ class MainActivity : AppCompatActivity() {
         val imgResId = R.drawable.singapore
         var resId = imgResId
 
-        var colorValue = ContextCompat.getColor(this, R.color.blanco)
-        var colorId = colorValue
-
         upload.setOnClickListener{
             if (resId == R.drawable.singapore)
                 resId = R.drawable.madidi
@@ -28,20 +25,9 @@ class MainActivity : AppCompatActivity() {
                 resId = R.drawable.singapore
 
             foto.setImageResource(resId)
-
             //foto.setBackgroundResource(R.drawable.madidi)
         }
 
-        fondo.setOnClickListener{
 
-            if (colorId == colorValue) {
-                colorValue = ContextCompat.getColor(this, R.color.plomo)
-                scroll.setBackgroundColor(colorValue)
-            }else
-            {
-                colorValue = ContextCompat.getColor(this, R.color.blanco)
-                scroll.setBackgroundColor(colorValue)
-            }
-        }
     }
 }
